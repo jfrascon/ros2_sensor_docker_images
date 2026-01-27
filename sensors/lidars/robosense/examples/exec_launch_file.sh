4e04fef7b7d57c686e816bbc7c5f7d95b5bb7ba2
jfrascon <jfrascon@gmail.com>
2026-01-27 00:12:47 +0000

    Refactor validation logic for robot_name and config_file in exec_launch_file.sh

--- sensors/lidars/robosense/examples/exec_launch_file.sh
+++ sensors/lidars/robosense/examples/exec_launch_file.sh
@@ -10,26 +10,39 @@ config_file="${CONFIG_FILE:-}"
 node_options="${NODE_OPTIONS:-}"
 logging_options="${LOGGING_OPTIONS:-}"
 
-# If a config file is provided, replace the '{{robot_prefix}}' placeholder with the actual robot prefix ("${robot_name}_")
-if [ -n "${config_file}" ] && [ -f "${config_file}" ]; then
-    # First, validate that robot_name is provided and valid.
-    [ -z "${robot_name}" ] && {
-        echo "Error: ROBOT_NAME environment variable is required" >&2
-        exit 1
-    }
+# Validate that robot_name is provided and valid.
+[ -z "${robot_name}" ] && {
+    echo "Error: ROBOT_NAME environment variable is required" >&2
+    exit 1
+}
 
-    # Ensure the regex checks only ASCII letters and digits, even if the system locale differs.
-    LC_ALL=C
+# Ensure the regex checks only ASCII letters and digits, even if the system locale differs.
+LC_ALL=C
 
-    # A name in ROS2 is valid if:
-    # - The first character is a letter (a-z, A-Z) or underscore (_), never a number.
-    # - The rest of the characters can be letters (a-z, A-Z), numbers (0-9) or underscore (_).
-    if ! [[ ${robot_name} =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
-        echo "Error: robot_name '${robot_name}' is invalid (first char cannot be a number: a-z, A-Z, _;" \
-            "rest: a-z, A-Z, 0-9, _)" >&2
-        exit 1
-    fi
+# A name in ROS2 is valid if:
+# - The first character is a letter (a-z, A-Z) or underscore (_), never a number.
+# - The rest of the characters can be letters (a-z, A-Z), numbers (0-9) or underscore (_).
+if ! [[ ${robot_name} =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
+    echo "Error: robot_name '${robot_name}' is invalid (first char cannot be a number: a-z, A-Z, _;" \
+        "rest: a-z, A-Z, 0-9, _)" >&2
+    exit 1
+fi
 
+# If the config_files is not provided, exit with error.
+[ -z "${config_file}" ] && {
+    echo "Error: The 'config_file' is required but not provided" >&2
+    exit 1
+}
+
+# If the config_file does not exist or is not a file, exit with error.
+[ ! -f "${config_file}" ] && {
+    echo "Error: The specified CONFIG_FILE '${config_file}' does not exist or is not a file" >&2
+    exit 1
+}
+
+# Optionally replace the '{{robot_prefix}}' placeholder with the actual robot prefix ("${robot_name}_").
+# Only generate a new config if the placeholder is present.
+if grep -q '{{robot_prefix}}' "${config_file}"; then
     robot_prefix="${robot_name}_"
     tmp_config_file="$(mktemp)"
     cp "${config_file}" "${tmp_config_file}"
